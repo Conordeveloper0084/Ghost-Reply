0c9a76bb7248c77b8f359a7ea923682f3d86d864
Conordeveloper0084 <conordev0084@gmail.com>
2025-12-24 14:41:00 +0500

    Updated

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BACKEND_URL = os.getenv("BACKEND_URL")
--- worker/config.py
+++ worker/config.py
@@ -10,11 +10,6 @@ TRIGGER_CACHE_TTL = int(os.getenv("TRIGGER_CACHE_TTL", 10))
 
 WORKER_ID = os.getenv("WORKER_ID", str(uuid.uuid4()))
 MAX_CLIENTS = int(os.getenv("MAX_CLIENTS", 50))
-
-# Maximum concurrent active tasks per worker
 MAX_ACTIVE_TASKS = int(os.getenv("MAX_ACTIVE_TASKS", 20))
 
-BACKEND_URL = os.getenv(
-    "BACKEND_URL",
-    "http://backend-server.railway.internal:8080"
-)
+BACKEND_URL = os.getenv("BACKEND_URL")
