d785e893f416b53b137de5d73b66a50d22b2732e
Conordeveloper0084 <conordev0084@gmail.com>
2025-12-24 14:31:09 +0500

    Updated

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-BACKEND_URL = os.getenv("BACKEND_URL", "http://backend:8000")
+BACKEND_URL = os.getenv("BACKEND_URL")
 PUBLIC_BACKEND_URL = os.getenv("PUBLIC_BACKEND_URL")
